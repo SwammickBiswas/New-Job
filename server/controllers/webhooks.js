89a72ef4f12e9b23636d5891f37bfef76f10bf47
SwammickBiswas <22052944@kiit.ac.in>
2025-03-01 11:34:41 +0530

    second commit

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -48,7 +48,7 @@ export const clerkWebHooks = async (req, res) => {
         break;
     }
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
+    console.log(error); 
+    res.status(500).json({ message: "Internal Server Error" }); 
   }
 };
